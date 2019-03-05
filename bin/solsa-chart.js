b09ffd90223cd5a2c6b4d69ea74eb63f043dac6a
Olivier Tardieu <tardieu@us.ibm.com>
2019-03-05 08:51:24 -0500

    Add .js extension to commands

--- bin/solsa-chart.js
+++ bin/solsa-chart.js
@@ -5,19 +5,19 @@ const minimist = require('minimist')
 const fs = require('fs')
 const archiver = require('archiver')
 
-function setupArchiver(chartName) {
-  var output = fs.createWriteStream('chartName' + '.tar.gz');
+function setupArchiver (chartName) {
+  var output = fs.createWriteStream('chartName' + '.tar.gz')
   var archive = archiver('tar', {
     gzip: true,
     zlib: { level: 9 }
-  });
+  })
 
   // listen for all archive data to be written
   // 'close' event is fired only when a file descriptor is involved
   output.on('close', function () {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-  });
+    console.log(archive.pointer() + ' total bytes')
+    console.log('archiver has been finalized and the output file descriptor has closed.')
+  })
 
   // good practice to catch warnings (ie stat failures and other non-blocking errors)
   archive.on('warning', function (err) {
@@ -25,41 +25,41 @@ function setupArchiver(chartName) {
       // log warning
     } else {
       // throw error
-      throw err;
+      throw err
     }
-  });
+  })
 
   // good practice to catch this error explicitly
   archive.on('error', function (err) {
-    throw err;
-  });
+    throw err
+  })
 
   // pipe archive data to the file
-  archive.pipe(output);
+  archive.pipe(output)
 
   return archive
 }
 
-function generateChartSkeleton(archive, app, version) {
+function generateChartSkeleton (archive, app, version) {
   const c = {
     apiVersion: 'v1',
     name: app.name,
     version: version
   }
-  archive.append(yaml.safeDump(c, { noArrayIndent: true }), { name: app.name+'/Chart.yaml' })
+  archive.append(yaml.safeDump(c, { noArrayIndent: true }), { name: app.name + '/Chart.yaml' })
 
-  return app.name+'/templates/'
+  return app.name + '/templates/'
 }
 
 const argv = minimist(process.argv.slice(2), {
-  default: { target: 'kubernetes', output: 'my-chart', version: '1.0.0'},
-  alias: { target: 't', output: 'o', version: 'v'},
+  default: { target: 'kubernetes', output: 'my-chart', version: '1.0.0' },
+  alias: { target: 't', output: 'o', version: 'v' },
   string: ['target', 'output', 'version']
-});
+})
 
-const archive = setupArchiver(argv.output);
-const theApp = require(require('path').resolve(argv._[0]));
-const templateDir = generateChartSkeleton(archive, theApp, argv.version);
-theApp._helm(archive, argv.target, templateDir);
+const archive = setupArchiver(argv.output)
+const theApp = require(require('path').resolve(argv._[0]))
+const templateDir = generateChartSkeleton(archive, theApp, argv.version)
+theApp._helm(archive, argv.target, templateDir)
 
-archive.finalize();
+archive.finalize()
