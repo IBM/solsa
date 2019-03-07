e8153151ec00a73550049c715316074da62608ac
Olivier Tardieu <tardieu@us.ibm.com>
2019-03-07 11:00:58 -0500

    Parse solsa-name --registry flag

--- bin/solsa-name.js
+++ bin/solsa-name.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
 
-let name = require(require('path').resolve(process.argv[2])).name.toLowerCase()
-console.log('solsa' + '-' + name)
+const minimist = require('minimist')
+
+const argv = minimist(process.argv.slice(2), {
+  default: { registry: process.env.REGISTRY },
+  alias: { registry: 'r' },
+  string: ['registry', 'r']
+})
+
+if (argv._.length !== 1) {
+  console.error('Usage:')
+  console.error('  solsa-name service [flags]')
+  console.error('Flags:')
+  console.error('  -r, --registry         name of the container registry')
+  process.exit(1)
+}
+
+let name = require(require('path').resolve(argv._[0])).name.toLowerCase()
+console.log((argv.registry ? argv.registry + '/' : '') + 'solsa' + '-' + name)
