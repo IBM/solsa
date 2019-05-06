bcabdccd2ceba387398d0039e992ab2a861c4ae1
David Grove <groved@us.ibm.com>
2019-05-06 12:44:10 -0400

    next refactor babystep; push most of the Service-specific code down

--- solsa.js
+++ solsa.js
@@ -26,39 +26,11 @@ let solsa = {
         svc.solsa.rank = global.__count++
       }
       svc.solsa.options.push(...arguments)
-      if (svc.solsa.raw || global.__yaml) return svc
-
-      let url
-      const filename = process.env.SOLSA_CONFIG || path.join(os.homedir(), '.solsa.yaml')
-      const config = fs.existsSync(filename) ? yaml.safeLoad(fs.readFileSync(filename, 'utf8')) : {}
-      const cluster = (config.clusters || []).find(cluster => cluster.name === process.env.SOLSA_CLUSTER) || {}
-      if (cluster.ingress && cluster.ingress.nodePort) {
-        url = `http://localhost:${cluster.ingress.nodePort + svc.solsa.rank}`
-      } else if (cluster.ingress && cluster.ingress.iks && cluster.ingress.iks.subdomain) {
-        if (cluster.nature === 'knative') {
-          url = `http://${svc.name}.default.${cluster.ingress.iks.subdomain}`
-        } else {
-          url = `https://${svc.name}.${cluster.ingress.iks.subdomain}`
-        }
+      if (svc.solsa.raw || global.__yaml) {
+        return svc
       } else {
-        url = `http://${svc.name}:${svc.port}`
+        return solsa.Service.rewriteRoutes(svc)
       }
-
-      for (let key of Object.getOwnPropertyNames(svc.constructor.prototype).filter(name => name !== 'constructor')) {
-        svc[key] = async function () {
-          return needle('post', url + '/' + key, arguments[0], { json: true })
-            .then(result => result.body)
-        }
-      }
-
-      for (let event of svc.events.eventNames()) {
-        svc.events.removeAllListeners(event)
-        svc.events.on(event, function () {
-          needle('post', url + '/' + event, arguments[0], { json: true })
-        })
-      }
-
-      return svc
     }
 
     constructor (solsaServiceArgs, raw, file = callSites()[1].getFileName()) {
@@ -71,11 +43,7 @@ let solsa = {
         this.port = solsaServiceArgs.port || 8080
       }
       this.events = new Events()
-      this.solsa = { raw, serviceReady: false, dependencies: [], secrets: {}, options: [], file }
-    }
-
-    async initializeService () {
-      this.serviceReady = true
+      this.solsa = { raw, dependencies: [], secrets: {}, options: [], file }
     }
 
     addDependency (dep) {
@@ -116,134 +84,7 @@ let solsa = {
     }
 
     async _yaml (archive) {
-      let env = await this._yamlMyDependencies(archive)
-
-      if (Object.getOwnPropertyNames(this.constructor.prototype).filter(name => name !== 'constructor').length === 0) return
-
-      const deployment = {
-        apiVersion: 'apps/v1',
-        kind: 'Deployment',
-        metadata: {
-          name: this.name,
-          labels: { 'solsa.ibm.com/name': this.name }
-        },
-        spec: {
-          replicas: 1,
-          selector: {
-            matchLabels: { 'solsa.ibm.com/name': this.name }
-          },
-          template: {
-            metadata: {
-              labels: { 'solsa.ibm.com/name': this.name }
-            },
-            spec: {
-              containers: [{
-                name: this.name,
-                image: solsaImage(this.constructor.name),
-                ports: [{ name: 'solsa', containerPort: this.port }],
-                env: Object.keys(env).map(key => Object.assign({ name: key }, env[key])),
-                livenessProbe: {
-                  tcpSocket: {
-                    port: this.port
-                  }
-                },
-                readinessProbe: {
-                  httpGet: {
-                    path: '/solsa/readinessProbe',
-                    port: this.port
-                  }
-                }
-              }]
-            }
-          }
-        }
-      }
-      archive.addResource(deployment, this.name + '-deployment.yaml', 'kubernetes')
-
-      const svc = {
-        apiVersion: 'v1',
-        kind: 'Service',
-        metadata: {
-          name: this.name,
-          labels: { 'solsa.ibm.com/name': this.name }
-        },
-        spec: {
-          type: 'ClusterIP',
-          ports: [{ name: 'solsa', port: this.port }],
-          selector: { 'solsa.ibm.com/name': this.name }
-        }
-      }
-      archive.addResource(svc, this.name + '-svc.yaml', 'kubernetes')
-
-      const ksvc = {
-        apiVersion: 'serving.knative.dev/v1alpha1',
-        kind: 'Service',
-        metadata: {
-          name: this.name
-        },
-        spec: {
-          runLatest: {
-            configuration: {
-              revisionTemplate: {
-                spec: {
-                  container: {
-                    image: solsaImage(this.constructor.name),
-                    env: Object.keys(env).map(key => Object.assign({ name: key }, env[key]))
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      archive.addResource(ksvc, this.name + '-svc.yaml', 'knative')
-    }
-
-    static serve () {
-      let options = JSON.parse(process.env.SOLSA_OPTIONS || '[]')
-      let port = parseInt(process.env.SOLSA_PORT, 10)
-
-      let svc = new this(...options)
-
-      svc.initializeService()
-
-      let express = require('express')
-      let app = express()
-      app.use(express.json())
-
-      for (let key of Object.getOwnPropertyNames(this.prototype).filter(name => name !== 'constructor')) {
-        app.post('/' + key, (request, response) => {
-          Promise.resolve().then(() => svc[key](request.body)).then(r => response.send(r), err => response.status(500).send((err && err.message) || 'Internal error'))
-        })
-      }
-
-      for (let key of svc.events.eventNames()) {
-        app.post('/' + key, (request, response) => {
-          Promise.resolve().then(() => svc.events.emit(key, request.body)).then(() => response.status(200).send('OK'), err => response.status(500).send((err && err.message) || 'Internal error'))
-        })
-      }
-
-      app.get('/solsa/readinessProbe', (_request, response) => {
-        if (svc.serviceReady) {
-          response.status(200).send('OK')
-        } else {
-          response.status(503).send('Service not ready')
-        }
-      })
-
-      app.post('/', (request, response) => {
-        Promise.resolve().then(() => {
-          svc.events.emit(request.body.event || 'any', request.body.payload)
-        }).then(() => response.status(200).send('OK'), err => response.status(500).send((err && err.message) || 'Internal error'))
-      })
-
-      app.listen(port, err => {
-        if (err) {
-          console.error(err)
-        } else {
-          console.log(`server is listening on ${port}`)
-        }
-      })
+      await this._yamlMyDependencies(archive)
     }
   }
 }
@@ -251,6 +92,176 @@ let solsa = {
 solsa.Service = class Service extends solsa.Resource {
   constructor (name, raw, file = callSites()[1].getFileName()) {
     super(name, raw, file)
+    this.serviceReady = false
+  }
+
+  async initializeService () {
+    this.serviceReady = true
+  }
+
+  async _yaml (archive) {
+    let env = await this._yamlMyDependencies(archive)
+
+    if (Object.getOwnPropertyNames(this.constructor.prototype).filter(name => name !== 'constructor').length === 0) return
+
+    const deployment = {
+      apiVersion: 'apps/v1',
+      kind: 'Deployment',
+      metadata: {
+        name: this.name,
+        labels: { 'solsa.ibm.com/name': this.name }
+      },
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: { 'solsa.ibm.com/name': this.name }
+        },
+        template: {
+          metadata: {
+            labels: { 'solsa.ibm.com/name': this.name }
+          },
+          spec: {
+            containers: [{
+              name: this.name,
+              image: solsaImage(this.constructor.name),
+              ports: [{ name: 'solsa', containerPort: this.port }],
+              env: Object.keys(env).map(key => Object.assign({ name: key }, env[key])),
+              livenessProbe: {
+                tcpSocket: {
+                  port: this.port
+                }
+              },
+              readinessProbe: {
+                httpGet: {
+                  path: '/solsa/readinessProbe',
+                  port: this.port
+                }
+              }
+            }]
+          }
+        }
+      }
+    }
+    archive.addResource(deployment, this.name + '-deployment.yaml', 'kubernetes')
+
+    const svc = {
+      apiVersion: 'v1',
+      kind: 'Service',
+      metadata: {
+        name: this.name,
+        labels: { 'solsa.ibm.com/name': this.name }
+      },
+      spec: {
+        type: 'ClusterIP',
+        ports: [{ name: 'solsa', port: this.port }],
+        selector: { 'solsa.ibm.com/name': this.name }
+      }
+    }
+    archive.addResource(svc, this.name + '-svc.yaml', 'kubernetes')
+
+    const ksvc = {
+      apiVersion: 'serving.knative.dev/v1alpha1',
+      kind: 'Service',
+      metadata: {
+        name: this.name
+      },
+      spec: {
+        runLatest: {
+          configuration: {
+            revisionTemplate: {
+              spec: {
+                container: {
+                  image: solsaImage(this.constructor.name),
+                  env: Object.keys(env).map(key => Object.assign({ name: key }, env[key]))
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    archive.addResource(ksvc, this.name + '-svc.yaml', 'knative')
+  }
+
+  static rewriteRoutes (svc) {
+    let url
+    const filename = process.env.SOLSA_CONFIG || path.join(os.homedir(), '.solsa.yaml')
+    const config = fs.existsSync(filename) ? yaml.safeLoad(fs.readFileSync(filename, 'utf8')) : {}
+    const cluster = (config.clusters || []).find(cluster => cluster.name === process.env.SOLSA_CLUSTER) || {}
+    if (cluster.ingress && cluster.ingress.nodePort) {
+      url = `http://localhost:${cluster.ingress.nodePort + svc.solsa.rank}`
+    } else if (cluster.ingress && cluster.ingress.iks && cluster.ingress.iks.subdomain) {
+      if (cluster.nature === 'knative') {
+        url = `http://${svc.name}.default.${cluster.ingress.iks.subdomain}`
+      } else {
+        url = `https://${svc.name}.${cluster.ingress.iks.subdomain}`
+      }
+    } else {
+      url = `http://${svc.name}:${svc.port}`
+    }
+
+    for (let key of Object.getOwnPropertyNames(svc.constructor.prototype).filter(name => name !== 'constructor')) {
+      svc[key] = async function () {
+        return needle('post', url + '/' + key, arguments[0], { json: true })
+          .then(result => result.body)
+      }
+    }
+
+    for (let event of svc.events.eventNames()) {
+      svc.events.removeAllListeners(event)
+      svc.events.on(event, function () {
+        needle('post', url + '/' + event, arguments[0], { json: true })
+      })
+    }
+
+    return svc
+  }
+
+  static serve () {
+    let options = JSON.parse(process.env.SOLSA_OPTIONS || '[]')
+    let port = parseInt(process.env.SOLSA_PORT, 10)
+
+    let svc = new this(...options)
+
+    svc.initializeService()
+
+    let express = require('express')
+    let app = express()
+    app.use(express.json())
+
+    for (let key of Object.getOwnPropertyNames(this.prototype).filter(name => name !== 'constructor')) {
+      app.post('/' + key, (request, response) => {
+        Promise.resolve().then(() => svc[key](request.body)).then(r => response.send(r), err => response.status(500).send((err && err.message) || 'Internal error'))
+      })
+    }
+
+    for (let key of svc.events.eventNames()) {
+      app.post('/' + key, (request, response) => {
+        Promise.resolve().then(() => svc.events.emit(key, request.body)).then(() => response.status(200).send('OK'), err => response.status(500).send((err && err.message) || 'Internal error'))
+      })
+    }
+
+    app.get('/solsa/readinessProbe', (_request, response) => {
+      if (svc.serviceReady) {
+        response.status(200).send('OK')
+      } else {
+        response.status(503).send('Service not ready')
+      }
+    })
+
+    app.post('/', (request, response) => {
+      Promise.resolve().then(() => {
+        svc.events.emit(request.body.event || 'any', request.body.payload)
+      }).then(() => response.status(200).send('OK'), err => response.status(500).send((err && err.message) || 'Internal error'))
+    })
+
+    app.listen(port, err => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(`server is listening on ${port}`)
+      }
+    })
   }
 }
 
