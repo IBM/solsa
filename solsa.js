b9297dcdb2f88c55addc9d67b7d421224d49a87e
Olivier Tardieu <tardieu@users.noreply.github.com>
2019-05-21 17:43:02 -0400

    Tweak

--- solsa.js
+++ solsa.js
@@ -2,4 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 // import all libs
-fs.readdirSync(path.join(__dirname, 'lib')).map(file => Object.assign(module.exports, require(path.join(__dirname, 'lib', file))))
+for (let file of fs.readdirSync(path.join(__dirname, 'lib'))) {
+  Object.assign(module.exports, require(path.join(__dirname, 'lib', file)))
+}
