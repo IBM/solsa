ed81afe1b2d2c0dbdd2786ef9d01c986bbeea89e
David Grove <groved@us.ibm.com>
2019-05-28 12:58:32 -0400

    1. sosla ==> solsa 2. allow install.sh to be run from any directory by adding path in sed command

--- install/install.sh
+++ install/install.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+SCRIPTDIR=$(cd $(dirname "$0") && pwd)
+
 SECRET=$(kubectl get secrets -n seed-operators seed-seed-registry -o jsonpath='{.data.\.dockerconfigjson}')
-sed "s/\$SECRET/$SECRET/" soslaNamespaceSetup.yaml | kubectl apply -f - $*
+sed "s/\$SECRET/$SECRET/" "$SCRIPTDIR/solsaNamespaceSetup.yaml" | kubectl apply -f - $*
