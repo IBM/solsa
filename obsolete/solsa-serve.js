a75eaec36526a3b9c2d9fa57bc13588a68b418a4
David Grove <groved@us.ibm.com>
2019-06-04 13:41:52 -0400

    housekeeping: remove obsolete subdir

--- obsolete/solsa-serve.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-require(require('path').resolve(process.argv[2])).serve()
--- obsolete/solsaNamespaceSetup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPTDIR=$(cd $(dirname "$0") && pwd)
-
-SECRET=$(kubectl get secrets -n seed-operators seed-seed-registry -o jsonpath='{.data.\.dockerconfigjson}')
-sed "s/\$SECRET/$SECRET/" "$SCRIPTDIR/solsaNamespaceSetup.yaml" | kubectl apply -f - $*
