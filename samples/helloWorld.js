1e80dc8202d3e52b141015124719124ee9dd5f59
David Grove <groved@us.ibm.com>
2019-06-04 16:00:47 -0400

    port must be explicitly provided

--- samples/helloWorld.js
+++ samples/helloWorld.js
@@ -2,5 +2,5 @@ const solsa = require('solsa')
 const bundle = new solsa.Bundle()
 module.exports = bundle
 
-bundle.helloWorld = new solsa.ContainerizedService({ name: 'hello-world', image: 'kn-helloworld' })
+bundle.helloWorld = new solsa.ContainerizedService({ name: 'hello-world', image: 'kn-helloworld', port: 8080 })
 bundle.ingress = new bundle.helloWorld.Ingress()
