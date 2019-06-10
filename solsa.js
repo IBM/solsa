51b2b75c65207033e5f44543c83f27f9791a4256
David Grove <groved@us.ibm.com>
2019-06-10 17:28:59 -0400

    make js-yaml available to clients as solsa.yaml

--- solsa.js
+++ solsa.js
@@ -5,3 +5,5 @@ const path = require('path')
 for (let file of fs.readdirSync(path.join(__dirname, 'lib'))) {
   Object.assign(module.exports, require(path.join(__dirname, 'lib', file)))
 }
+
+module.exports.yaml = require('js-yaml')
