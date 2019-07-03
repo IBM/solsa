a8064cf0f45af2409637d11b8b4c51f82a8bfffc
Olivier Tardieu <tardieu@users.noreply.github.com>
2019-07-03 16:35:54 -0400

    Add test for generated service resource

---
+++ test/yaml/service.js
@@ -0,0 +1,3 @@
+const solsa = require('solsa')
+
+module.exports = new solsa.v1.Service({ name: 'hello', type: 'ClusterIP', ports: [{ port: 8080, targetPort: 8080 }], spec: { selector: 'solsa.ibm.com/pod: hello' } })
