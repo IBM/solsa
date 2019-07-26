4fd6761f94e5d0c5b892501af773ca3ec5aa28f4
Olivier Tardieu <tardieu@users.noreply.github.com>
2019-07-26 10:43:40 -0400

    Add license headers

--- codegen/codegen.js
+++ codegen/codegen.js
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
 wget https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/swagger.json
 node codegen.js > ../src/synthetic.ts
 */
@@ -36,6 +52,23 @@ for (let resource of Object.values(swagger.definitions)) {
     level[schema.kind] = new Schema(schema)
   }
 }
+
+console.log(`/*
+ * Copyright 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+`)
 
 console.log(`import { Core } from './core'`)
 console.log(`import { dynamic } from './helpers'`)
