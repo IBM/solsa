603b09da8165aeef0046dcc79d8f90230055bb10
David Grove <groved@us.ibm.com>
2019-09-26 14:15:00 -0400

    Deployment.getService() improvements

--- src/core-ext.ts
+++ src/core-ext.ts
@@ -14,28 +14,49 @@
  * limitations under the License.
  */
 
-import { core, apps, extensions } from './core'
+import { core, apps, extensions, meta } from './core'
 
 declare module './core' {
-    namespace apps {
-        namespace v1 {
-            interface Deployment {
-              getService (): core.v1.Service
-            }
-        }
-        namespace v1beta2 {
-            interface Deployment {
-              getService (): core.v1.Service
-            }
-        }
+  namespace apps {
+    namespace v1 {
+      interface Deployment {
+        getService (): core.v1.Service
+      }
     }
-    namespace extensions {
-        namespace v1beta1 {
-            interface Deployment {
-              getService (): core.v1.Service
-            }
-        }
+    namespace v1beta2 {
+      interface Deployment {
+        getService (): core.v1.Service
+      }
     }
+  }
+  namespace extensions {
+    namespace v1beta1 {
+      interface Deployment {
+        getService (): core.v1.Service
+      }
+    }
+  }
+}
+
+namespace coreExt {
+  export function servicePorts (containers: core.v1.Container[]): core.v1.ServicePort[] {
+    let ports: core.v1.ServicePort[] = []
+    containers.forEach(function (c: core.v1.Container) {
+      if (c.ports !== undefined) {
+        c.ports.forEach(function (cp: core.v1.ContainerPort) {
+          ports.push({ name: cp.name, port: cp.containerPort, targetPort: cp.containerPort, protocol: cp.protocol })
+        })
+      }
+    })
+    return ports
+  }
+
+  export function augmentLabels (meta: meta.v1.ObjectMeta, toAdd: { [k: string]: string }) {
+    if (meta.labels === undefined) {
+      meta.labels = {}
+    }
+    Object.assign(meta.labels, toAdd)
+  }
 }
 
 apps.v1.Deployment.prototype.getService = function () {
@@ -50,25 +71,36 @@ apps.v1.Deployment.prototype.getService = function () {
   }
 
   // Ensure all labels in selector are included in the PodSpec metadata
-  // TODO: Should we really do this, or should we throw an error if the user screwed up and this isn't already true?
   const selector = this.spec.selector.matchLabels
   if (this.spec.template.metadata === undefined) {
-    this.spec.template.metadata = { labels: {} }
-  } else if (this.spec.template.metadata.labels === undefined) {
-    this.spec.template.metadata.labels = {}
+    this.spec.template.metadata = { }
   }
-  Object.assign(this.spec.template.metadata.labels, selector)
+  coreExt.augmentLabels(this.spec.template.metadata, selector)
 
-  let ports: core.v1.ServicePort[] = []
-  this.spec.template.spec.containers.forEach(function (c: core.v1.Container) {
-    if (c.ports !== undefined) {
-      c.ports.forEach(function (cp: core.v1.ContainerPort) {
-        ports.push({ name: cp.name, port: cp.containerPort, targetPort: cp.containerPort, protocol: cp.protocol })
-      })
-    }
-  })
-
+  const ports = coreExt.servicePorts(this.spec.template.spec.containers)
   return new core.v1.Service({ metadata: { name: this.metadata.name }, spec: { ports, selector, type: 'ClusterIP' } })
 }
-extensions.v1beta1.Deployment.prototype.getService = apps.v1.Deployment.prototype.getService
-apps.v1beta2.Deployment.prototype.getService = apps.v1.Deployment.prototype.getService
+
+extensions.v1beta1.Deployment.prototype.getService = function () {
+  if (this.spec.template.spec === undefined) {
+    throw new Error('Cannot get a service on Deployment without a spec')
+  }
+  if (this.metadata.name === undefined) {
+    throw new Error('Deployment must have a `name` to derive a service')
+  }
+  if (this.spec.selector === undefined) {
+    this.spec.selector = { matchLabels: { 'solsa.ibm.com/pod' : this.metadata.name } }
+  }
+
+  // Ensure all labels in selector are included in the PodSpec metadata
+  const selector = this.spec.selector.matchLabels
+  if (this.spec.template.metadata === undefined) {
+    this.spec.template.metadata = { }
+  }
+  coreExt.augmentLabels(this.spec.template.metadata, selector!)
+
+  const ports = coreExt.servicePorts(this.spec.template.spec.containers)
+  return new core.v1.Service({ metadata: { name: this.metadata.name }, spec: { ports, selector, type: 'ClusterIP' } })
+}
+
+apps.v1beta2.Deployment.prototype.getService = extensions.v1beta1.Deployment.prototype.getService
